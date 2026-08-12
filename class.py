46e987dc71c7f8d34e0bda4e64d265d08566e5d6
Chetan Lohia <chetan.lohia2073@gmail.com>
2026-08-13 00:39:51 +0530

    create a class

--- class.py
+++ class.py
@@ -1,9 +1,16 @@
+#Syntax for making class is 
+#   class class_name:
 
 class Employee:
     name= "Chetan"
     salary = 100000
     language = "python"
 
+# Syntax for making object is 
+# variabe = class_name()
 
 ch= Employee()
+
+# to use the attribute of a class we use '.' 
+# variable.attribute
 print(ch.name,ch.salary,ch.language)
